64d4227e1ef2051892a25bd528e3332a2e589a6c
Will <willhy@gmail.com>
2015-11-28 13:54:17 -0600

    Remove early return

--- server/remove-user.sh
+++ server/remove-user.sh
@@ -6,8 +6,6 @@ BACKUP_NAME=$1
 
 [[ ! "$BACKUP_NAME" =~ ^[a-zA-Z0-9]+$ ]] && echo "Pass name of backup. (a-Z0-9 e.g. willBackup01)" && exit 1
 
-exit 1
-
 read -r -p "Are you sure? All data will be deleted. [Type YES to proceed] " response
 [[ "$response" != "YES" ]] && exit 1
 
