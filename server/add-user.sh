fa9d84d6354920e0f78eb89b2127f987ce4b3d34
Will <willhy@gmail.com>
2015-11-27 20:44:08 -0600

    Script to add user to server

---
+++ server/add-user.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+BACKUP_NAME=$1
+
+useradd --no-create-home --home-dir /tmp -s /bin/bash -G backupusers $BACKUP_NAME
+
+wget https://raw.githubusercontent.com/williamhaley/configs/master/create-jail.sh -P /tmp/
+
+PATH_TO_JAIL=/mnt/storage1/backup-jail/$BACKUP_NAME
+
+bash /tmp/create-jail.sh $PATH_TO_JAIL
+chmod 775 $PATH_TO_JAIL/mnt
+chown $BACKUP_NAME:$BACKUP_NAME $PATH_TO_JAIL/mnt
+
+ssh-keygen -f $PATH_TO_JAIL/etc/$BACKUP_NAME-SSH -t rsa -N ''
+
+echo "Key for $BACKUP_USER. Added here at $(date)." >> /etc/ssh/authorized_keys
+cat $PATH_TO_JAIL/etc/$BACKUP_NAME-SSH.pub >> /etc/ssh/authorized_keys
+
+
